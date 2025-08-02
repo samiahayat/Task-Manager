31ac5f99bb323f1dd9e839d617342633fc9f3cd6
samiahayat <samiahayat95@gmail.com>
2025-08-02 17:56:23 +0500

    Delete Config.py

--- Config.py
+++
@@ -1,3 +0,0 @@
-MONGO_URI = "mongodb+srv://samia123:aimas1234@cluster0.5issyjn.mongodb.net/TaskProject?retryWrites=true&w=majority&appName=Cluster0"
-DB_NAME = "TaskProject"
-COLLECTION_NAME = "tasks"
